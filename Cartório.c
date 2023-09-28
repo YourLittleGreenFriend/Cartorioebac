0a140f6f2a1e5740f5dde450612d6f825f0f1158
YourLittleGreenFriend <146121377+YourLittleGreenFriend@users.noreply.github.com>
2023-09-28 11:25:30 -0300

    Add files via upload

---
+++ Cartório.c
@@ -0,0 +1,148 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <locale.h> 
+#include <string.h> 
+		
+int registro()
+{
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	printf("Digite o CPF a ser cadastrado: ");
+	scanf("%s", cpf);
+	
+	strcpy(arquivo, cpf); 
+	
+	FILE *file; 
+	file = fopen(arquivo, "w"); 
+	fprintf(file,cpf); 
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: ");
+	scanf("%s",nome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o sobrenome a ser cadastrado: ");
+	scanf("%s",sobrenome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o cargo a ser cadastrado: ");
+	scanf("%s",cargo);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,cargo);
+	fclose(file);	
+	
+    system("pause");
+
+}
+
+int consulta()
+{
+    setlocale(LC_ALL, "Portuguese");
+    char cpf[40];
+    char conteudo[200];
+    
+    
+    printf("Digite o cpf a ser consultado: ");
+    scanf("%s",cpf);
+    
+    FILE *file;
+    file = fopen(cpf,"r");
+    
+    if(file==NULL)
+{
+     printf("Não foi possível abrir o arquivo, não localizado!.\n");
+}
+     while(fgets(conteudo, 200, file) !=NULL){
+                           printf("\nEssas são as informaiões do usuário");
+                           printf("%s", conteudo);
+                           printf("\n\n");
+}
+
+     system("pause");
+}
+
+int deletar()
+{
+	printf("Você escolheu deletar nomes!\n");
+	system("pause");	
+}
+
+
+	
+int main()
+	{
+	int opcao=0; 
+	int laco=1;
+	
+	for(laco=1;laco=1;)
+	{
+
+		system("cls");
+
+		setlocale(LC_ALL, "Portuguese"); 
+			
+		printf("### Cartório da EBAC ###\n\n");
+		printf("Escolha a opção desejada do menu\n\n");
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n"); 
+		printf("\t4 - Sair do Sistema\n\n");
+		printf("Opção: ");
+				
+		scanf("%d", &opcao);
+		
+		
+		system("cls");
+	
+		switch(opcao)
+		
+		{
+			case 1:
+			registro();
+			break;
+			
+			case 2:
+			consulta();
+			break;
+		
+			case 3:
+			deletar();
+			break;
+			
+			case 4:
+            printf("Obrigado por utilizar o sistema!\n");
+            return 0;
+            break;
+		
+			default:
+			printf("Essa opção não está disponivel!\n");
+			system("pause");
+			break;
+		} 
+		
+			
+	}	
+}
